fe441ffdf6c3fc8baa9fdec64b8ec5314cfa3946
rayapetrova <rayapetrova_a18@schoolmath.eu>
2022-05-28 21:32:29 +0300

    btn

--- LibraryProject/Main.Designer.cs
+++ LibraryProject/Main.Designer.cs
@@ -58,17 +58,19 @@
             this.panel1.Controls.Add(this.FavouriteLabelMain);
             this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
             this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(236, 634);
+            this.panel1.Size = new System.Drawing.Size(313, 780);
             this.panel1.TabIndex = 0;
             // 
             // label10
             // 
             this.label10.AutoSize = true;
             this.label10.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label10.Location = new System.Drawing.Point(92, 575);
+            this.label10.Location = new System.Drawing.Point(123, 708);
+            this.label10.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label10.Name = "label10";
-            this.label10.Size = new System.Drawing.Size(45, 18);
+            this.label10.Size = new System.Drawing.Size(58, 25);
             this.label10.TabIndex = 2;
             this.label10.Text = "User";
             this.label10.Click += new System.EventHandler(this.UserPanel_Click);
@@ -77,9 +79,10 @@
             // 
             this.WTRLabelMain.AutoSize = true;
             this.WTRLabelMain.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.WTRLabelMain.Location = new System.Drawing.Point(49, 124);
+            this.WTRLabelMain.Location = new System.Drawing.Point(65, 153);
+            this.WTRLabelMain.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.WTRLabelMain.Name = "WTRLabelMain";
-            this.WTRLabelMain.Size = new System.Drawing.Size(116, 18);
+            this.WTRLabelMain.Size = new System.Drawing.Size(143, 25);
             this.WTRLabelMain.TabIndex = 1;
             this.WTRLabelMain.Text = "Want to read";
             this.WTRLabelMain.Click += new System.EventHandler(this.WTRLabelMain_Click);
@@ -88,9 +91,10 @@
             // 
             this.TSRLabelMain.AutoSize = true;
             this.TSRLabelMain.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.TSRLabelMain.Location = new System.Drawing.Point(26, 64);
+            this.TSRLabelMain.Location = new System.Drawing.Point(35, 79);
+            this.TSRLabelMain.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.TSRLabelMain.Name = "TSRLabelMain";
-            this.TSRLabelMain.Size = new System.Drawing.Size(167, 18);
+            this.TSRLabelMain.Size = new System.Drawing.Size(206, 25);
             this.TSRLabelMain.TabIndex = 1;
             this.TSRLabelMain.Text = "Time spent reading";
             this.TSRLabelMain.Click += new System.EventHandler(this.TSRLabelMain_Click);
@@ -99,9 +103,10 @@
             // 
             this.LibraryLabelMain.AutoSize = true;
             this.LibraryLabelMain.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.LibraryLabelMain.Location = new System.Drawing.Point(74, 155);
+            this.LibraryLabelMain.Location = new System.Drawing.Point(99, 191);
+            this.LibraryLabelMain.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.LibraryLabelMain.Name = "LibraryLabelMain";
-            this.LibraryLabelMain.Size = new System.Drawing.Size(63, 18);
+            this.LibraryLabelMain.Size = new System.Drawing.Size(83, 25);
             this.LibraryLabelMain.TabIndex = 1;
             this.LibraryLabelMain.Text = "Library";
             this.LibraryLabelMain.Click += new System.EventHandler(this.LibraryLabelMain_Click);
@@ -110,9 +115,10 @@
             // 
             this.FavouriteLabelMain.AutoSize = true;
             this.FavouriteLabelMain.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.FavouriteLabelMain.Location = new System.Drawing.Point(37, 95);
+            this.FavouriteLabelMain.Location = new System.Drawing.Point(49, 117);
+            this.FavouriteLabelMain.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.FavouriteLabelMain.Name = "FavouriteLabelMain";
-            this.FavouriteLabelMain.Size = new System.Drawing.Size(137, 18);
+            this.FavouriteLabelMain.Size = new System.Drawing.Size(172, 25);
             this.FavouriteLabelMain.TabIndex = 1;
             this.FavouriteLabelMain.Text = "Favourite books";
             this.FavouriteLabelMain.Click += new System.EventHandler(this.FavouriteLabelMain_Click);
@@ -121,27 +127,30 @@
             // 
             this.panel3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(241)))), ((int)(((byte)(242)))), ((int)(((byte)(246)))));
             this.panel3.Controls.Add(this.label3);
-            this.panel3.Location = new System.Drawing.Point(242, 47);
+            this.panel3.Location = new System.Drawing.Point(323, 58);
+            this.panel3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.panel3.Name = "panel3";
-            this.panel3.Size = new System.Drawing.Size(835, 179);
+            this.panel3.Size = new System.Drawing.Size(1113, 220);
             this.panel3.TabIndex = 1;
             // 
             // label3
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(315, 70);
+            this.label3.Location = new System.Drawing.Point(420, 86);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(249, 18);
+            this.label3.Size = new System.Drawing.Size(306, 25);
             this.label3.TabIndex = 1;
             this.label3.Text = "Welcome (Welcoming Image)";
             // 
             // LibraryBtn
             // 
             this.LibraryBtn.Font = new System.Drawing.Font("Verdana", 12F);
-            this.LibraryBtn.Location = new System.Drawing.Point(334, 453);
+            this.LibraryBtn.Location = new System.Drawing.Point(445, 481);
+            this.LibraryBtn.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.LibraryBtn.Name = "LibraryBtn";
-            this.LibraryBtn.Size = new System.Drawing.Size(264, 111);
+            this.LibraryBtn.Size = new System.Drawing.Size(352, 137);
             this.LibraryBtn.TabIndex = 1;
             this.LibraryBtn.Text = "Library";
             this.LibraryBtn.UseVisualStyleBackColor = true;
@@ -150,9 +159,10 @@
             // Favourite
             // 
             this.Favourite.Font = new System.Drawing.Font("Verdana", 12F);
-            this.Favourite.Location = new System.Drawing.Point(334, 316);
+            this.Favourite.Location = new System.Drawing.Point(445, 303);
+            this.Favourite.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Favourite.Name = "Favourite";
-            this.Favourite.Size = new System.Drawing.Size(264, 111);
+            this.Favourite.Size = new System.Drawing.Size(352, 137);
             this.Favourite.TabIndex = 4;
             this.Favourite.Text = "Favourite books";
             this.Favourite.UseVisualStyleBackColor = true;
@@ -161,9 +171,10 @@
             // TSR
             // 
             this.TSR.Font = new System.Drawing.Font("Verdana", 12F);
-            this.TSR.Location = new System.Drawing.Point(702, 316);
+            this.TSR.Location = new System.Drawing.Point(936, 389);
+            this.TSR.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.TSR.Name = "TSR";
-            this.TSR.Size = new System.Drawing.Size(264, 111);
+            this.TSR.Size = new System.Drawing.Size(352, 137);
             this.TSR.TabIndex = 5;
             this.TSR.Text = "Time spent reading";
             this.TSR.UseVisualStyleBackColor = true;
@@ -172,9 +183,10 @@
             // WTR
             // 
             this.WTR.Font = new System.Drawing.Font("Verdana", 12F);
-            this.WTR.Location = new System.Drawing.Point(702, 453);
+            this.WTR.Location = new System.Drawing.Point(936, 558);
+            this.WTR.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.WTR.Name = "WTR";
-            this.WTR.Size = new System.Drawing.Size(264, 111);
+            this.WTR.Size = new System.Drawing.Size(352, 137);
             this.WTR.TabIndex = 2;
             this.WTR.Text = "Want to read";
             this.WTR.UseVisualStyleBackColor = true;
@@ -187,9 +199,10 @@
             this.CloseButton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
             this.CloseButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.CloseButton.ForeColor = System.Drawing.Color.Transparent;
-            this.CloseButton.Location = new System.Drawing.Point(1042, 5);
+            this.CloseButton.Location = new System.Drawing.Point(1389, 6);
+            this.CloseButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.CloseButton.Name = "CloseButton";
-            this.CloseButton.Size = new System.Drawing.Size(35, 35);
+            this.CloseButton.Size = new System.Drawing.Size(47, 43);
             this.CloseButton.TabIndex = 2;
             this.CloseButton.UseVisualStyleBackColor = false;
             this.CloseButton.Click += new System.EventHandler(this.CloseButton_Click);
@@ -197,17 +210,18 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(1087, 621);
+            this.label1.Location = new System.Drawing.Point(1449, 764);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(0, 13);
+            this.label1.Size = new System.Drawing.Size(0, 17);
             this.label1.TabIndex = 6;
             // 
             // Main
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
-            this.ClientSize = new System.Drawing.Size(1089, 634);
+            this.ClientSize = new System.Drawing.Size(1452, 780);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.WTR);
             this.Controls.Add(this.TSR);
@@ -218,6 +232,7 @@
             this.Controls.Add(this.panel1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Main";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = " ";
