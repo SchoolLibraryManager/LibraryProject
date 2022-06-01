d9db147532094de7f39aff8946afd12c238ed393
rayapetrova <rayapetrova_a18@schoolmath.eu>
2022-06-01 17:21:04 +0300

    userspage

--- LibraryProject/Properties/Resources.Designer.cs
+++ LibraryProject/Properties/Resources.Designer.cs
@@ -323,6 +323,16 @@ namespace LibraryProject.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap user1 {
+            get {
+                object obj = ResourceManager.GetObject("user1", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap VapcarovBook {
             get {
                 object obj = ResourceManager.GetObject("VapcarovBook", resourceCulture);
--- LibraryProject/UserPanel.Designer.cs
+++ LibraryProject/UserPanel.Designer.cs
@@ -29,9 +29,46 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(UserPanel));
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.button1 = new System.Windows.Forms.Button();
             this.CloseButton = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.WTRLabelMain = new System.Windows.Forms.Label();
+            this.TSRLabelMain = new System.Windows.Forms.Label();
+            this.FavouriteLabelMain = new System.Windows.Forms.Label();
+            this.panel5 = new System.Windows.Forms.Panel();
+            this.panel6 = new System.Windows.Forms.Panel();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel2.SuspendLayout();
+            this.panel5.SuspendLayout();
+            this.panel6.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Sitka Small", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(638, 269);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(200, 42);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "User\'s name ";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::LibraryProject.Properties.Resources.user1;
+            this.pictureBox1.Location = new System.Drawing.Point(412, 198);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(168, 162);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 7;
+            this.pictureBox1.TabStop = false;
             // 
             // button1
             // 
@@ -40,9 +77,10 @@
             this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
             this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button1.ForeColor = System.Drawing.Color.Transparent;
-            this.button1.Location = new System.Drawing.Point(-1, 0);
+            this.button1.Location = new System.Drawing.Point(11, 8);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(35, 35);
+            this.button1.Size = new System.Drawing.Size(47, 43);
             this.button1.TabIndex = 5;
             this.button1.UseVisualStyleBackColor = false;
             this.button1.Click += new System.EventHandler(this.GoBack_Click);
@@ -54,26 +92,171 @@
             this.CloseButton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
             this.CloseButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.CloseButton.ForeColor = System.Drawing.Color.Transparent;
-            this.CloseButton.Location = new System.Drawing.Point(1054, 0);
+            this.CloseButton.Location = new System.Drawing.Point(870, -12);
+            this.CloseButton.Margin = new System.Windows.Forms.Padding(4);
             this.CloseButton.Name = "CloseButton";
-            this.CloseButton.Size = new System.Drawing.Size(35, 35);
+            this.CloseButton.Size = new System.Drawing.Size(47, 43);
             this.CloseButton.TabIndex = 4;
             this.CloseButton.UseVisualStyleBackColor = false;
             this.CloseButton.Click += new System.EventHandler(this.CloseButton_Click);
             // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Sitka Small", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(640, 331);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(138, 29);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "User\'s email";
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.Tan;
+            this.panel2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel2.Controls.Add(this.label5);
+            this.panel2.Controls.Add(this.CloseButton);
+            this.panel2.Location = new System.Drawing.Point(327, 45);
+            this.panel2.Margin = new System.Windows.Forms.Padding(5);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(848, 110);
+            this.panel2.TabIndex = 9;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Sitka Small", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label5.ForeColor = System.Drawing.SystemColors.Control;
+            this.label5.Location = new System.Drawing.Point(232, 10);
+            this.label5.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(406, 88);
+            this.label5.TabIndex = 1;
+            this.label5.Text = "User\'s Page";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Sitka Small", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label7.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.label7.Location = new System.Drawing.Point(640, 595);
+            this.label7.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(88, 29);
+            this.label7.TabIndex = 14;
+            this.label7.Text = "Library";
+            // 
+            // WTRLabelMain
+            // 
+            this.WTRLabelMain.AutoSize = true;
+            this.WTRLabelMain.Font = new System.Drawing.Font("Sitka Small", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.WTRLabelMain.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.WTRLabelMain.Location = new System.Drawing.Point(640, 540);
+            this.WTRLabelMain.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.WTRLabelMain.Name = "WTRLabelMain";
+            this.WTRLabelMain.Size = new System.Drawing.Size(144, 29);
+            this.WTRLabelMain.TabIndex = 11;
+            this.WTRLabelMain.Text = "Want to read";
+            // 
+            // TSRLabelMain
+            // 
+            this.TSRLabelMain.AutoSize = true;
+            this.TSRLabelMain.Font = new System.Drawing.Font("Sitka Small", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.TSRLabelMain.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.TSRLabelMain.Location = new System.Drawing.Point(640, 433);
+            this.TSRLabelMain.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.TSRLabelMain.Name = "TSRLabelMain";
+            this.TSRLabelMain.Size = new System.Drawing.Size(208, 29);
+            this.TSRLabelMain.TabIndex = 12;
+            this.TSRLabelMain.Text = "Time spent reading";
+            // 
+            // FavouriteLabelMain
+            // 
+            this.FavouriteLabelMain.AutoSize = true;
+            this.FavouriteLabelMain.Font = new System.Drawing.Font("Sitka Small", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.FavouriteLabelMain.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.FavouriteLabelMain.Location = new System.Drawing.Point(640, 486);
+            this.FavouriteLabelMain.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.FavouriteLabelMain.Name = "FavouriteLabelMain";
+            this.FavouriteLabelMain.Size = new System.Drawing.Size(173, 29);
+            this.FavouriteLabelMain.TabIndex = 13;
+            this.FavouriteLabelMain.Text = "Favourite books";
+            // 
+            // panel5
+            // 
+            this.panel5.BackColor = System.Drawing.Color.Tan;
+            this.panel5.Controls.Add(this.button1);
+            this.panel5.Location = new System.Drawing.Point(2, 5);
+            this.panel5.Name = "panel5";
+            this.panel5.Size = new System.Drawing.Size(200, 811);
+            this.panel5.TabIndex = 15;
+            // 
+            // panel6
+            // 
+            this.panel6.BackColor = System.Drawing.Color.Tan;
+            this.panel6.Controls.Add(this.button2);
+            this.panel6.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.panel6.Location = new System.Drawing.Point(1274, 5);
+            this.panel6.Name = "panel6";
+            this.panel6.Size = new System.Drawing.Size(200, 819);
+            this.panel6.TabIndex = 16;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.White;
+            this.button2.BackgroundImage = global::LibraryProject.Properties.Resources.CloseProgramm;
+            this.button2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.ForeColor = System.Drawing.Color.Transparent;
+            this.button2.Location = new System.Drawing.Point(143, 8);
+            this.button2.Margin = new System.Windows.Forms.Padding(4);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(47, 43);
+            this.button2.TabIndex = 5;
+            this.button2.UseVisualStyleBackColor = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Sitka Small", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.label3.Location = new System.Drawing.Point(640, 710);
+            this.label3.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(88, 29);
+            this.label3.TabIndex = 17;
+            this.label3.Text = "Log out";
+            // 
             // UserPanel
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1089, 634);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.CloseButton);
+            this.BackColor = System.Drawing.Color.OldLace;
+            this.ClientSize = new System.Drawing.Size(1477, 828);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel6);
+            this.Controls.Add(this.panel5);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.WTRLabelMain);
+            this.Controls.Add(this.TSRLabelMain);
+            this.Controls.Add(this.FavouriteLabelMain);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "UserPanel";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "UserPanel";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.panel5.ResumeLayout(false);
+            this.panel6.ResumeLayout(false);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -81,5 +264,18 @@
 
         private System.Windows.Forms.Button CloseButton;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label WTRLabelMain;
+        private System.Windows.Forms.Label TSRLabelMain;
+        private System.Windows.Forms.Label FavouriteLabelMain;
+        private System.Windows.Forms.Panel panel5;
+        private System.Windows.Forms.Panel panel6;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label3;
     }
 }
